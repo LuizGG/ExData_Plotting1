83b7c7d8a8b83b699d4b8e8d7a8a2ec3250a12c6
LuizG <luiz.giordani@outlook.com>
2016-02-18 04:11:31 -0200

    Renaming some files

--- Plot4.R
+++ Plot4.R
@@ -37,7 +37,7 @@ plot(Sub_metering_1, type = "l", ylab = "Energy sub metering", xaxt = "n", xlab 
 lines(Sub_metering_2, col = "red")
 lines(Sub_metering_3, col = "blue")
 axis(1, at = c(0, 1500, 2900), labels = c("Thu", "Fri", "Sat"))
-legend("topright", lty = c(1,1,1) ,col = c("black", "red", "blue"), bty = "n" , legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend(x = 600, y = 40 , lty = c(1,1,1) ,col = c("black", "red", "blue"), bty = "n" , legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 ### Plot 4.4
 plot(Global_reactive_power, type = "l",  xaxt = "n", xlab = "datetime")
